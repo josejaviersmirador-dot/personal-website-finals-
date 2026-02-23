6b756b90d708d1171658fe39dba6609207bcec69
Jose Javier Mirador <josejaviersmirador@gmail.com>
2026-02-23 16:24:23 +0000

    chnages

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/personal-website-finals/', 
-  plugins: [vue()],
+  base: '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
